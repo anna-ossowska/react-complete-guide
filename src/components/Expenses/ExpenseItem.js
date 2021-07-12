1f70836f7e92a8cad94d42069d0ab722d981d796
Anna Ossowska <ossowska.anna1@gmail.com>
2021-07-12 17:11:49 +0200

    Rendering lists of data

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-const ExpenseItem = (props) => {
-  // registering a state value
-  const [title, setTitle] = useState(props.title);
-  // console.log('Expense Item evaluated');
-
-  const clickHandler = function () {
-    console.log(title);
-    setTitle('Updated');
-  };
-
+const ExpenseItem = props => {
   return (
     <Card className="expense-item">
       <div className="expense-item__description">
         {/* Child component */}
         <ExpenseDate date={props.date} />
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
