766934c202feb3c0199e93f2edc5e294e27b108a
Anna Ossowska <ossowska.anna1@gmail.com>
2021-07-15 23:22:22 +0200

    Adding State and preserving data

--- 04-Project-3/src/App.js
+++ 04-Project-3/src/App.js
@@ -1,17 +1,38 @@
+import React, { useState } from 'react';
 import './App.css';
 import User from './components/User';
 import UserForm from './components/UserForm';
 import Card from './UI/Card';
+
+const DUMMY_DATA = [
+  {
+    userName: 'John',
+    age: '30',
+    id: '1',
+  },
+  {
+    userName: 'Jane',
+    age: '21',
+    id: '2',
+  },
+];
+
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const addUserHandler = function (user) {
+    setUsers((prev) => [user, ...prev]);
+  };
+
+  console.log(users);
   return (
     <div>
       <Card>
-        <UserForm />
+        <UserForm onAddUser={addUserHandler} />
       </Card>
+
       <Card>
-        <div>
-          <User />
-        </div>
+        <User />
       </Card>
     </div>
   );
--- 04-Project-3/src/components/UserForm.js
+++ 04-Project-3/src/components/UserForm.js
@@ -1,19 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './UserForm.module.css';
 import Button from '../UI/Button';
 
+// Pass data from userForm to App
+
 const UserForm = (props) => {
+  const [inputName, setInputName] = useState('');
+  const [inputAge, setInputAge] = useState('');
+
+  const onChangeNameHandler = function (e) {
+    setInputName(e.target.value);
+  };
+
+  const onChangeAgeHandler = function (e) {
+    setInputAge(e.target.value + '');
+  };
+
+  const clearInputFields = function () {
+    setInputName('');
+    setInputAge('');
+  };
+
+  const onSubmitHandler = function (e) {
+    e.preventDefault();
+    console.log(inputName, inputAge);
+
+    const user = {
+      userName: inputName,
+      userAge: inputAge,
+      id: Math.random().toString(),
+    };
+    // Pass data up to App.js
+    props.onAddUser(user);
+    clearInputFields();
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmitHandler}>
         <div className={styles['form-controls']}>
           <div>
             <label>User Name</label>
-            <input type="text" />
+            <input
+              value={inputName}
+              type="text"
+              onChange={onChangeNameHandler}
+            />
           </div>
           <div>
             <label>Age</label>
-            <input type="number" />
+            <input
+              value={inputAge}
+              type="number"
+              onChange={onChangeAgeHandler}
+            />
           </div>
           <div className="btn-control">
             <Button type="submit">Add User</Button>
