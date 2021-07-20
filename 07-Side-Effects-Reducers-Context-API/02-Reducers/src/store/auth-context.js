60169f1f8cd67d7750a340efa417e04113df43d6
Anna Ossowska <ossowska.anna1@gmail.com>
2021-07-20 17:49:02 +0200

    Forwarding functions through context instead of props

--- 07-Side-Effects-Reducers-Context-API/02-Reducers/src/store/auth-context.js
+++ 07-Side-Effects-Reducers-Context-API/02-Reducers/src/store/auth-context.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 // Creating a Context object
 // When React renders a component that subscribes to this object, it will read the current context value from the closest matching Provider above it in the tree.
-const AuthContext = React.createContext({ isLoggedIn: false });
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+});
 
 export default AuthContext;
