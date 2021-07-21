1858001523ab071a0236f048d6fc7e8c02dc0874
Anna Ossowska <ossowska.anna1@gmail.com>
2021-07-21 19:35:27 +0200

    Forward Refs

--- 07-Side-Effects-Reducers-Context-API/02-Reducers/src/components/Login/Login.js
+++ 07-Side-Effects-Reducers-Context-API/02-Reducers/src/components/Login/Login.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useReducer, useContext } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useReducer,
+  useContext,
+  useRef,
+} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -63,6 +69,9 @@ const Login = (props) => {
 
   const ctx = useContext(AuthContext);
 
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   useEffect(() => {
     console.log('EFFECT RUNNING');
 
@@ -115,13 +124,20 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    ctx.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      ctx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           isValid={emailIsValid}
           onChange={emailChangeHandler}
           onBlur={validateEmailHandler}
@@ -132,6 +148,7 @@ const Login = (props) => {
         />
 
         <Input
+          ref={passwordInputRef}
           isValid={passwordIsValid}
           onChange={passwordChangeHandler}
           onBlur={validatePasswordHandler}
@@ -139,10 +156,11 @@ const Login = (props) => {
           type="password"
           id="password"
           value={passwordState.value}
+          autoComplete="on"
         />
 
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
--- 07-Side-Effects-Reducers-Context-API/02-Reducers/src/components/UI/Input/Input.js
+++ 07-Side-Effects-Reducers-Context-API/02-Reducers/src/components/UI/Input/Input.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useRef, useImperativeHandle } from 'react';
 import classes from './Input.module.css';
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    // object containing data that would be used from outside
+    return {
+      focus: activate,
+    };
+  });
+
   return (
     <div
       className={`${classes.control} ${
@@ -10,14 +23,16 @@ const Input = (props) => {
     >
       <label htmlFor={props.id}>{props.label}</label>
       <input
+        ref={inputRef}
         type={props.type}
         id={props.id}
         value={props.value}
         onChange={props.onChange}
         onBlur={props.onBlur}
+        autoComplete={props.autoComplete}
       />
     </div>
   );
-};
+});
 
 export default Input;
