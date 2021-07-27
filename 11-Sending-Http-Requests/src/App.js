f686a762b0d9b91f6b45548caa39d98102f41401
Anna Ossowska <ossowska.anna1@gmail.com>
2021-07-27 19:54:21 +0200

    Sending a POST request

--- 11-Sending-Http-Requests/src/App.js
+++ 11-Sending-Http-Requests/src/App.js
@@ -2,19 +2,24 @@ import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  // ------- GET request -------
   const fetchMoviesHandler = useCallback(async function () {
     setIsLoading(true);
+
     // Clearing any prev errors
     setError(null);
 
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch(
+        'https://react-http-92c39-default-rtdb.europe-west1.firebasedatabase.app/movies.json'
+      );
       console.log(response);
 
       if (!response.ok) {
@@ -23,15 +28,18 @@ function App() {
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          releaseDate: movieData.release_date,
-          openingText: movieData.opening_crawl,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (err) {
       setError(err.message);
     }
@@ -39,38 +47,27 @@ function App() {
     setIsLoading(false);
   }, []);
 
-  // Fetching data as soon as the component loads
-  // Fetching data from API is a side effect, thus, such a state should be handled with useEffect
-  // We refer to fetchMoviesHandler as a dependency because in the future we might alter the function
-  // in a way it relies on some external state, and we want to avoid any potential bugs
-  // However, this solution produces another potential bug: fns are objects, and with each App component re-execution, we recreate the fn from scratch
-  // To avoid creating an infinite loop, we must wrap the async function with useCallback()
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  // fetch('https://swapi.dev/api/films/').then((response) => {
-  //   if (response.ok) {
-  //     return response.json().then((data) => {
-  //       console.log(data.results);
+  // ------- POST REQUEST -------
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      'https://react-http-92c39-default-rtdb.europe-west1.firebasedatabase.app/movies.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
 
-  //       const transformedMovies = data.results.map((movieData) => {
-  //         return {
-  //           id: movieData.episode_id,
-  //           title: movieData.title,
-  //           releaseDate: movieData.release_date,
-  //           openingText: movieData.opening_crawl,
-  //         };
-  //       });
-  //       setMovies(transformedMovies);
-  //     });
-  //   }
-  // });
-
-  // {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-  // {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
-  // {isLoading && <p>Loading...</p>}
-  // {!isLoading && error && <p>{error}</p>}
+    // Firebase sends data back in JSON format
+    const data = await response.json();
+    console.log(data);
+  }
 
   let content = <p>Found no movies</p>;
 
@@ -89,6 +86,9 @@ function App() {
   return (
     <React.Fragment>
       <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>{content}</section>
@@ -97,3 +97,69 @@ function App() {
 }
 
 export default App;
+
+// function App() {
+//   const [movies, setMovies] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [error, setError] = useState(null);
+
+//   const fetchMoviesHandler = useCallback(async function () {
+//     setIsLoading(true);
+//     // Clearing any prev errors
+//     setError(null);
+
+//     try {
+//       const response = await fetch('https://swapi.dev/api/films/');
+//       console.log(response);
+
+//       if (!response.ok) {
+//         throw new Error('Something went wrong!');
+//       }
+
+//       const data = await response.json();
+
+//       const transformedMovies = data.results.map((movieData) => {
+//         return {
+//           id: movieData.episode_id,
+//           title: movieData.title,
+//           releaseDate: movieData.release_date,
+//           openingText: movieData.opening_crawl,
+//         };
+//       });
+//       setMovies(transformedMovies);
+//     } catch (err) {
+//       setError(err.message);
+//     }
+
+//     setIsLoading(false);
+//   }, []);
+
+// Fetching data as soon as the component loads
+// Fetching data from API is a side effect, thus, such a state should be handled with useEffect
+// We refer to fetchMoviesHandler as a dependency because in the future we might alter the function
+// in a way it relies on some external state, and we want to avoid any potential bugs
+// However, this solution produces another potential bug: fns are objects, and with each App component re-execution, we recreate the fn from scratch
+// To avoid creating an infinite loop, we must wrap the async function with useCallback()
+
+// useEffect(() => {
+//   fetchMoviesHandler();
+// }, [fetchMoviesHandler]);
+
+// ------- Handling Promises with 'then' -------
+// fetch('https://swapi.dev/api/films/').then((response) => {
+//   if (response.ok) {
+//     return response.json().then((data) => {
+//       console.log(data.results);
+
+//       const transformedMovies = data.results.map((movieData) => {
+//         return {
+//           id: movieData.episode_id,
+//           title: movieData.title,
+//           releaseDate: movieData.release_date,
+//           openingText: movieData.opening_crawl,
+//         };
+//       });
+//       setMovies(transformedMovies);
+//     });
+//   }
+// });
