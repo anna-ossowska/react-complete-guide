588dc51881615be616440fdc565af39639e1667c
Anna Ossowska <ossowska.anna1@gmail.com>
2021-07-27 14:30:30 +0200

    Loading state

--- 11-Sending-Http-Requests/src/App.js
+++ 11-Sending-Http-Requests/src/App.js
@@ -5,9 +5,11 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
     try {
+      setIsLoading(true);
       const response = await fetch('https://swapi.dev/api/films/');
 
       if (response.ok) {
@@ -22,6 +24,7 @@ function App() {
           };
         });
         setMovies(transformedMovies);
+        setIsLoading(false);
       }
     } catch (err) {
       console.error(err);
@@ -52,7 +55,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
