794c125511e0db2a5d9e842a433821e0a5208f30
Anna Ossowska <ossowska.anna1@gmail.com>
2021-07-29 17:33:37 +0200

    2 approaches for managing the form submission: useState vs useRef

--- 13-Working-with-Forms/src/components/SimpleInput.js
+++ 13-Working-with-Forms/src/components/SimpleInput.js
@@ -1,9 +1,39 @@
+import { useState, useRef } from 'react';
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const nameInputRef = useRef('');
+
+  const nameInputChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(enteredName);
+
+    const enteredNameValue = nameInputRef.current.value;
+    console.log(enteredNameValue);
+
+    // Resetting input field upon submission:
+
+    // Not ideal solution, DO NOT manipulate the DOM
+    // nameInputRef.current.value = '';
+
+    setEnteredName('');
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          ref={nameInputRef}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
