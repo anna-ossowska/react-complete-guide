0ac1afd15bc2695bd2c2ff274613199625c8a498
Anna Ossowska <ossowska.anna1@gmail.com>
2021-07-30 18:57:59 +0200

    Exercises: input validation

--- 13-Working-with-Forms/src/components/BasicForm.js
+++ 13-Working-with-Forms/src/components/BasicForm.js
@@ -1,22 +1,70 @@
+import { useState } from 'react';
+
 const BasicForm = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const [nameIsTouched, setNameisTouched] = useState(false);
+
+  const nameIsValid = enteredName !== '';
+  const nameInputHasError = !nameIsValid && nameIsTouched;
+
+  let formIsValid = false;
+
+  if (nameIsValid) {
+    formIsValid = true;
+  }
+
+  const nameChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const nameBlurHandler = () => {
+    setNameisTouched(true);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setNameisTouched(true);
+
+    if (!nameIsValid) return;
+
+    console.log(enteredName);
+    console.log(nameIsValid);
+
+    setEnteredName('');
+    setNameisTouched(false);
+  };
+
+  const nameInputClasses = nameInputHasError
+    ? 'form-control invalid'
+    : 'form-control';
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className="control-group">
+        <div className={nameInputClasses}>
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            id="name"
+            value={enteredName}
+            onChange={nameChangeHandler}
+            onBlur={nameBlurHandler}
+          />
+          {nameInputHasError && (
+            <p className="error-text">Field cannot be empty</p>
+          )}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className="form-control">
+          <label htmlFor="name">Last Name</label>
+          <input type="text" id="name" />
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">E-Mail Address</label>
+        <input type="text" id="name" />
       </div>
-      <div className='form-actions'>
-        <button>Submit</button>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
