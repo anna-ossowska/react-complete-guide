6b75424932141cfad9a05e2a0e9c972b7f06d31e
Anna Ossowska <ossowska.anna1@gmail.com>
2021-08-05 12:05:41 +0200

    Using an action creator thunk

--- 16-Advanced-Redux/01/src/App.js
+++ 16-Advanced-Redux/01/src/App.js
@@ -5,7 +5,7 @@ import Notification from './components/UI/Notification';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { Fragment, useEffect } from 'react';
-import { uiActions } from './store/ui-slice';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
 
@@ -20,51 +20,13 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const cartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data',
-        })
-      );
-      const response = await fetch(
-        'https://react-http-92c39-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!',
-        })
-      );
-    };
-
-    // If it returns, code does not procede to the next lines responsible for making the PUT request
-    // It blocks the cart from being sent when the Effect executes for the first time, so when application starts
     if (isInitial) {
       isInitial = false;
       return;
     }
-
-    cartData().catch((err) => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed.',
-        })
-      );
-    });
+    // Before we always dispatched action creators - fns that return an action object with a type, and so on
+    // Here we dispatch a fn that return another fn
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
@@ -85,3 +47,53 @@ function App() {
 }
 
 export default App;
+
+/*
+useEffect(() => {
+  const cartData = async () => {
+    dispatch(
+      uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data',
+      })
+    );
+    const response = await fetch(
+      'https://react-http-92c39-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
+      {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Sending cart data failed.');
+    }
+
+    dispatch(
+      uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Sent cart data successfully!',
+      })
+    );
+  };
+
+  // If it returns, code does not procede to the next lines responsible for making the PUT request
+  // It blocks the cart from being sent when the Effect executes for the first time, so when application starts
+  if (isInitial) {
+    isInitial = false;
+    return;
+  }
+
+  cartData().catch((err) => {
+    dispatch(
+      uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: 'Sending cart data failed.',
+      })
+    );
+  });
+}, [cart, dispatch]);
+*/
--- 16-Advanced-Redux/01/src/store/cart-slice.js
+++ 16-Advanced-Redux/01/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { uiActions } from './ui-slice';
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -42,6 +43,53 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCartData = (cartData) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data',
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        'https://react-http-92c39-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cartData),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        })
+      );
+    } catch (err) {
+      cartData().catch((err) => {
+        dispatch(
+          uiActions.showNotification({
+            status: 'error',
+            title: 'Error!',
+            message: 'Sending cart data failed.',
+          })
+        );
+      });
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 
