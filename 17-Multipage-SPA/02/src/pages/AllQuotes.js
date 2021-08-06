9053e9d9eaba7743ca545cb456504f8d62dd4f49
Anna Ossowska <ossowska.anna1@gmail.com>
2021-08-06 16:23:17 +0200

    Outputting data on the Details page

--- 17-Multipage-SPA/02/src/pages/AllQuotes.js
+++ 17-Multipage-SPA/02/src/pages/AllQuotes.js
@@ -1,5 +1,4 @@
 import { Fragment } from 'react';
-import { Link } from 'react-router-dom';
 import QuoteList from '../components/quotes/QuoteList';
 
 const AllQuotes = () => {
--- 17-Multipage-SPA/02/src/pages/QuoteDetail.js
+++ 17-Multipage-SPA/02/src/pages/QuoteDetail.js
@@ -1,15 +1,25 @@
 import { Fragment } from 'react';
 import { Route, useParams } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const DUMMY_QUOTES = [
+    { id: 'q1', author: 'John', text: 'Random quote 1' },
+    { id: 'q2', author: 'Jane', text: 'Random quote 2' },
+  ];
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found.</p>;
+  }
+
   return (
     <Fragment>
-      <h1>Quote detail</h1>
-      <p>{params.quoteId}</p>
-
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
