8764f5d2b66ce503842e183da3d576b0d322426b
Anna Ossowska <ossowska.anna1@gmail.com>
2021-08-21 14:19:07 +0200

    Working with the Prompt component

--- 17-Multipage-SPA/02/src/pages/NewQuote.js
+++ 17-Multipage-SPA/02/src/pages/NewQuote.js
@@ -4,6 +4,7 @@ import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
   const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
     history.push('/quotes');
