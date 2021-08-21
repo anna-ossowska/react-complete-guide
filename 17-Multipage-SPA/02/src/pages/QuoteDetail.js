b2e00ee3da477424043c18a372944564b63ba3d1
Anna Ossowska <ossowska.anna1@gmail.com>
2021-08-21 19:02:28 +0200

    Working with nested routes

--- 17-Multipage-SPA/02/src/pages/QuoteDetail.js
+++ 17-Multipage-SPA/02/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import { Route, useParams } from 'react-router-dom';
+import { Route, useParams, Link } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 
@@ -24,6 +24,14 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link to={`/quotes/${params.quoteId}/comments`} className="btn--flat">
+            Comments
+          </Link>
+        </div>
+      </Route>
+
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
