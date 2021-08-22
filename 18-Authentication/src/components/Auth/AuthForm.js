9c1d59674ecf994b8e7652eeccd50c6fa0980ba7
Anna Ossowska <ossowska.anna1@gmail.com>
2021-08-22 18:41:09 +0200

    Adding user Signup

--- 18-Authentication/src/components/Auth/AuthForm.js
+++ 18-Authentication/src/components/Auth/AuthForm.js
@@ -1,30 +1,72 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const eneteredPassword = passwordInputRef.current.value;
+
+    console.log(enteredEmail);
+
+    if (isLogin) {
+    } else {
+      fetch(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCXnEo1Yig7-efQluf-mhOCal_Ed8C8ck0',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: eneteredPassword,
+            returnSecureToken: true,
+          }),
+        }
+      ).then((res) => {
+        if (res.ok) {
+          //....
+        } else {
+          return res.json().then((data) => {
+            console.log(data);
+          });
+        }
+      });
+    }
+  };
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
