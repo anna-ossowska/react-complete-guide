1af49eb3587a8e02baa18be35be17f612b405aed
Anna Ossowska <ossowska.anna1@gmail.com>
2021-08-23 11:24:06 +0200

    Managing the Auth state with Context

---
+++ 18-Authentication/src/store/auth-context.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const AuthContext = React.createContext({
+  token: '',
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [token, setToken] = useState(null);
+
+  // cast to Boolean
+  const userIsLoggedIn = !!token;
+
+  const loginHanlder = (token) => {
+    setToken(token);
+  };
+
+  const logoutHandler = () => {
+    setToken(null);
+  };
+
+  const contextValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHanlder,
+    logout: logoutHandler,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
