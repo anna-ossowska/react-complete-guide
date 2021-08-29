e3b8310355f30930499a0e9b95287b64d5a683e2
Anna Ossowska <ossowska.anna1@gmail.com>
2021-08-29 19:17:24 +0200

    Grouping tests together with Test Suites

--- 20-Unit-Tests/src/components/Greeting.test.js
+++ 20-Unit-Tests/src/components/Greeting.test.js
@@ -1,14 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
-test('renders Hello World as a text', () => {
-  // Arrange
-  render(<Greeting />);
+// Grouping tests together with Test Suites
+describe('Greeting component', () => {
+  test('renders Hello World as a text', () => {
+    // Arrange
+    render(<Greeting />);
 
-  // Act
-  // ...N/A
+    // Act
+    // ...N/A
 
-  // Assert
-  const helloWorldEl = screen.getByText('hello world', { exact: false });
-  expect(helloWorldEl).toBeInTheDocument();
+    // Assert
+    const helloWorldEl = screen.getByText('hello world', { exact: false });
+    expect(helloWorldEl).toBeInTheDocument();
+  });
 });
